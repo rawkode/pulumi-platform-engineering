8e2a06b33753f8746580ffc9ee0568d83e29417d
David Flanagan <david@rawkode.dev>
2022-06-21 16:22:54 +0000

    feat: added github actions

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as civo from "@pulumi/civo";
+
+const network = new civo.Network("workshop", {
+	label: "workshop",
+});
+
+const firewall = new civo.Firewall("workshop", {
+	networkId: network.id,
+});
+
+const kubernetesCluster = new civo.KubernetesCluster("workshop", {
+	networkId: network.id,
+	firewallId: firewall.id,
+	pools: {
+		size: "g4s.kube.medium",
+		nodeCount: 3,
+	},
+});
