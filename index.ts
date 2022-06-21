917346ecd5b7ad22311bb8b77e9a098cb206e835
David Flanagan <david@rawkode.dev>
2022-06-21 16:38:48 +0000

    feat: allow developers to onboard to platform

--- index.ts
+++ index.ts
@@ -1,19 +1,35 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as civo from "@pulumi/civo";
+import * as kubernetes from "@pulumi/kubernetes";
+import { PulumiOperator } from "./operator";
 
 const network = new civo.Network("workshop", {
-	label: "workshop",
+  label: "workshop",
 });
 
 const firewall = new civo.Firewall("workshop", {
-	networkId: network.id,
+  networkId: network.id,
 });
 
 const kubernetesCluster = new civo.KubernetesCluster("workshop", {
-	networkId: network.id,
-	firewallId: firewall.id,
-	pools: {
-		size: "g4s.kube.medium",
-		nodeCount: 3,
-	},
+  networkId: network.id,
+  firewallId: firewall.id,
+  pools: {
+    size: "g4s.kube.medium",
+    nodeCount: 3,
+  },
 });
+
+export const kubeconfig = kubernetesCluster.kubeconfig;
+
+const operator = new PulumiOperator("workshop", {
+  provider: new kubernetes.Provider("civo", {
+    kubeconfig: kubernetesCluster.kubeconfig,
+  }),
+});
+
+const myProject = operator.addRepository(
+  "nginx",
+  "https://github.com/pulumi/workshops",
+  "/cicd-with-gha-and-pulumi-operator/gitops"
+);
